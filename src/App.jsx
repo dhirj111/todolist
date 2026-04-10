33a16e5886705e4295f0783b63718c37cd7f9bd6
dhirj111 <jdreamcreations@gmail.com>
2026-04-10 13:20:08 +0530

    project initiliazation  , made a header component , included that in main page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import Header from "./mycomponent/header";
+// import heroImg from "./assets/hero.png";
+import "./App.css";
+import header from "./mycomponent/header";
+
+function App() {
+  return (
+
+       <Header/>  
+  
+
+  );
+}
+
+export default App;
